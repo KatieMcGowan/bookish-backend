77ee36d662fecceb42f17b82d2e5616c2dfee623
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-10-19 11:04:01 -0700

    Fixes getid user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.get("/auth-endpoint", auth, ctrl.users.authEndpoint)
 router.get("/:id", ctrl.users.show);
 router.post("/", ctrl.users.create);
 router.post("/login", ctrl.users.verify)
-router.post("/getid", ctrl.users.getid)
+router.get("/getid", ctrl.users.getid)
 router.put("/:id", ctrl.users.update);
 
 module.exports = router;
